cea11abb08b3c6bfd9dfe8354dcdd78847d8ea4c
Andrew Reed <andrew.reed@roswellpark.org>
2024-02-02 18:18:35 -0500

    initial commit

---
+++ hikedata.js
@@ -0,0 +1,120 @@
+export var hikeData ={
+    "weeks": [
+    {
+        "week_number": 1,
+        "hikes": [
+            {
+                "name": "Pfeiffer Nature Center: Inner Loop",
+                "image": "./img/wk01/IMG_0038.jpg",
+                "location": "Portville, NY",
+                "mileage": 0.41,
+                "date": "2024-01-01",
+                "duration": {
+                    "minutes": 16
+                }
+            },
+            {
+                "name": "Erie County Reforestation Area",
+                "image": "./img/wk01/IMG_0035.jpg",
+                "location": "East Concord, NY",
+                "mileage": 2.92,
+                "date": "2024-01-01",
+                "duration": {
+                    "hours": 1,
+                    "minutes": 8
+                }
+            },
+            {
+                "name": "Tift Nature Preserve",
+                "image": "./img/wk01/IMG_0032.jpg",
+                "location": "Buffalo, NY",
+                "mileage": 2.71,
+                "date": "2024-01-07",
+                "duration": {
+                    "minutes": 54
+                }
+            },
+            {
+                "name": "18 Mile Creek",
+                "image": "./img/wk01/IMG_0033.jpg",
+                "location": "Hamburg, NY",
+                "mileage": 4,
+                "date": "2024-01-07",
+                "duration": {
+                    "hours": 1,
+                    "minutes": 26
+                }
+            },
+            {
+                "name": "Franklin Gulf County Park",
+                "image": "./img/wk01/IMG_0034.jpg",
+                "location": "North Collins, NY",
+                "mileage": 2.04,
+                "date": "2024-01-07",
+                "duration": {
+                    "minutes": 51
+                }
+            }
+        ]
+    },
+    {
+        "week_number": 2,
+        "hikes": [
+        ]
+    },
+    {
+        "week_number": 3,
+        "hikes": [
+            {
+                "name": "Nature View Park",
+                "image": "./img/wk03/IMG_0135.jpg",
+                "location": "Amherst, NY",
+                "mileage": 4.0,
+                "date": "2024-01-21",
+                "duration": {
+                    "hours": 1,
+                    "minutes": 42
+                }
+            },
+            {
+                "name": "Reinstein Woods",
+                "image": "./img/wk03/IMG_0136.jpg",
+                "location": "Depew, NY",
+                "mileage": 1.8,
+                "date": "2024-01-21",
+                "duration": {
+                    "hours": 1,
+                    "minutes": 27
+                }
+            }
+        ]
+    },
+    {
+        "week_number": 4,
+        "hikes": [
+            {
+                "name": "Holcomb and Ross Pond Loops",
+                "image": "./img/wk04/IMG_0000.jpg",
+                "location": "Gowanda, NY",
+                "mileage": 3.86,
+                "date": "2024-01-27",
+                "duration": {
+                    "hours": 1,
+                    "minutes": 24
+                }
+            },
+            {
+                "name": "Sprague Brook County Park",
+                "image": "./img/wk04/IMG_0000.jpg",
+                "location": "Glenwood, NY",
+                "mileage": 9.26,
+                "date": "2024-01-27",
+		        "duration": {
+                    "hours": 3,
+                    "minutes": 22
+                }
+            }
+        ]
+    }
+]
+};
---
+++ script.js
@@ -0,0 +1,106 @@
+import { hikeData } from './hikedata.js';
+
+
+// Function to calculate total miles, total hikes, total duration, and remaining miles
+function calculateStats() {
+    let totalMiles = 0;
+    let totalHikes = 0;
+    let totalDurationInMinutes = 0;
+    let weeksInYear = 52;
+    let goalMiles = 500;
+    let currentWeek =  hikeData.weeks.length;
+    const weeksRemaining = weeksInYear - currentWeek;
+
+    // Loop through weeks
+    hikeData.weeks.forEach((week) => {
+        // Loop through hikes in each week
+        week.hikes.forEach((hike) => {
+            totalMiles += hike.mileage;
+            totalHikes++;
+            totalDurationInMinutes += (hike.duration.hours || 0) * 60 + (hike.duration.minutes || 0);
+        });
+
+        // Generate HTML for the week
+        const weekHTML = `
+            <div class="weekly-row" >
+                <div class="overview grid-hack">
+                    <div class="inner">
+                        <div class="wk-number">Week ${week.week_number}</div>
+                        <div class="wk-miles">${week.hikes.reduce((acc, hike) => acc + hike.mileage, 0).toFixed(2)} M</div>
+                        <div class="wk-trails">${week.hikes.length} T</div>
+                        <div class="wk-expand">
+                            <span class="closed">+</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="detail grid-hack">
+                    <div class="inner">
+                        <header class="">
+                            <div class="wk-number"><span class="label">Week</span><span class="number">${week.week_number}</span></div>
+                            <div class="wk-miles"><span class="number">${week.hikes.reduce((acc, hike) => acc + hike.mileage, 0).toFixed(2)}</span><span class="label"> Miles</span></div>
+                            <div class="wk-trails"><span class="number">${week.hikes.length}</span><span class="label"> Trails</span></div>
+                            <div>X</div>
+                        </header>
+                        <div class="trail-list">
+                            ${week.hikes.map((hike) => `
+                            <div class="trail">
+                                <div class="trail-image"><img src="${hike.image}" alt="Map of ${hike.name}" /></div>
+                                <div class="trail-details">
+                                    <div class="trail-name"><h3>${hike.name}</h3></div>
+                                    <div class="trail-location">${hike.location}</div>
+                                    <div class="trail-length">${hike.mileage.toFixed(2)}</div>
+                                    <div class="trail-time">${hike.duration.hours || 0}hr ${hike.duration.minutes || 0}min</div>
+                                    <div class="trail-date">${hike.date}</div>
+                                </div>
+                            </div>
+                            `).join('')}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        // Append the generated HTML to the document
+        document.getElementById('weekly-table').insertAdjacentHTML('beforeend', weekHTML);
+    });
+
+
+    // Convert total duration to hours with decimal places
+    const totalDurationInHours = totalDurationInMinutes / 60;
+
+    // Calculate remaining miles
+    const remainingMiles = goalMiles - totalMiles;
+
+    // Calculate percent complete
+    const percentComplete = (totalMiles / goalMiles) * 100;
+
+    // Calculate remaining miles per remaining weeks
+    const milesPerWeek = remainingMiles/weeksRemaining;
+
+    // Display the results in the HTML document
+    document.getElementById('currentWeek').textContent = currentWeek;
+    document.getElementById('number-miles-completed').textContent = totalMiles.toFixed(1);
+    document.getElementById('number-trails').textContent = totalHikes;
+    document.getElementById('number-hours').textContent = totalDurationInHours.toFixed(1);
+    document.getElementById('number-miles-remaining').textContent = remainingMiles.toFixed(0);
+    document.getElementById('number-percent-finished').textContent = percentComplete.toFixed(0);
+    document.getElementById('number-mile-split').textContent = milesPerWeek.toFixed(2);
+    document.getElementById('remaining-weeks').textContent = weeksRemaining.toFixed(0);
+}
+
+// Call the function to calculate and display statistics
+calculateStats();
+
+//Collapse of weekly rows
+const divs = document.querySelectorAll('.weekly-row');
+function arse() {
+    divs.forEach((element) => element.classList.remove('active'));
+}
+divs.forEach(el => el.addEventListener('click', event => {
+    if(event.target.closest(".weekly-row").classList.contains("active")){
+        event.target.closest(".weekly-row").classList.remove('active');
+    }else{
+        arse();
+        event.target.closest(".weekly-row").classList.add('active');
+    }
+}));
